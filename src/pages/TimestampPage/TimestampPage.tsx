e9e9d4f4640aeee3b8ae349277449a42adf07610
Artem Onyshchenko <tema.onischenko@gmail.com>
2026-02-20 13:53:12 +0200

    timestamp page

--- src/pages/TimestampPage/TimestampPage.tsx
+++ src/pages/TimestampPage/TimestampPage.tsx
@@ -1,11 +1,31 @@
+import { Form, Input, Select } from "antd";
 import moment from "moment-timezone"
 
+interface FormOptions {
+    label: string;
+    value: string;
+}
+const dateTypeInputs: FormOptions[] = [
+    { label: "Timestamp", value: "timestamp" },
+    { label: 'Date Time Picker', value: "dateTimePicker" },
+]
+
 export default function Timestamp() {
+    const [form] = Form.useForm();
     const date = moment();
+    const onFinish = (values: any) => {
+        console.log(values);
+    }
     return (
         <div className="w-full max-w-[800px] min-w-[600px] flex flex-col bg-white gap-1 p-4 rounded shadow">
-            <h2>Welcome to the Timestamp Page</h2>
+            <Form form={form} onFinish={onFinish} layout="inline" variant="underlined">
+                <Form.Item label="Timestamp" name="timestamp" initialValue={date.valueOf()}><Input /></Form.Item>
+                <Form.Item label="Date Type" name="dateType" initialValue="timestamp">
+                    <Select options={dateTypeInputs} />
+                </Form.Item>
+            </Form>
             <div className="w-full flex flex-col px-4 py-2 bg-gray-100 divide-gray-400 divide-y">
+                
                 <div className="w-full flex leading-10 px-4">
                     <div>Timestamp:</div>
                     <div className="flex-1"></div>
